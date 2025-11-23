9d22677b973f8079615156f1c5354396e157c4f3
ayushisgreat2006 <pokimon0000999988887777@gmail.com>
2025-11-23 15:02:49 +0530

    Update ytdl.py

--- utils/ytdl.py
+++ utils/ytdl.py
@@ -1,39 +1,16 @@
-# utils/ytdl.py
-import os
-import asyncio
-from yt_dlp import YoutubeDL
-from config.config import DOWNLOADS, YTDL_FORMAT
+from callsmusic.converter import YTDLConverter
+from typing import Optional
 
-YDL_OPTS = {
-    "format": YTDL_FORMAT,
-    "outtmpl": os.path.join(DOWNLOADS, "%(id)s.%(ext)s"),
-    "nocheckcertificate": True,
-    "quiet": True,
-    "no_warnings": True,
-    "ignoreerrors": True,
-    "noplaylist": True,
-    "geo_bypass": True,
-    "nocheckcertificate": True,
-}
-
-async def extract_info(url_or_query: str):
-    loop = asyncio.get_event_loop()
-    def _extract():
-        with YoutubeDL({"format": "bestaudio/best", "quiet": True}) as ydl:
-            # If looks like URL -> download info; else search
-            if url_or_query.startswith("http"):
-                info = ydl.extract_info(url_or_query, download=False)
-            else:
-                info = ydl.extract_info(f"ytsearch1:{url_or_query}", download=False)['entries'][0]
-            return info
-    return await loop.run_in_executor(None, _extract)
-
-async def download_audio(info: dict):
-    loop = asyncio.get_event_loop()
-    def _download():
-        with YoutubeDL(YDL_OPTS) as ydl:
-            res = ydl.extract_info(info.get("webpage_url") or info.get("url"), download=True)
-            # res contains keys id, ext
-            filename = ydl.prepare_filename(res)
-            return filename, res
-    return await loop.run_in_executor(None, _download)
+class YouTubeDL:
+    """YouTube download wrapper"""
+    
+    @staticmethod
+    async def get_stream_url(url: str) -> Optional[str]:
+        """Get direct stream URL without downloading"""
+        info = await YTDLConverter.extract_info(url)
+        return info.get("url") if info else None
+    
+    @staticmethod
+    async def download(url: str) -> Optional[str]:
+        """Download audio file"""
+        return await YTDLConverter.download_audio(url)
