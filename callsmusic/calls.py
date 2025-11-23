4347db6bc04fdea80cf46f919649c832c729ce8f
ayushisgreat2006 <pokimon0000999988887777@gmail.com>
2025-11-23 14:59:27 +0530

    Update calls.py

--- callsmusic/calls.py
+++ callsmusic/calls.py
@@ -1,10 +1,40 @@
-# callsmusic/calls.py
-from pytgcalls import PyTgCalls
 from pyrogram import Client
-from config.config import ASSISTANT_STRING, API_ID, API_HASH, ASSISTANT_ID
-from callsmusic.player import Player
-import asyncio
+from pytgcalls import PyTgCalls
+from config import Config
+import logging
 
-def init_calls(assistant_app: Client):
-    pytgcalls = PyTgCalls(assistant_app)
-    return pytgcalls
+logger = logging.getLogger(__name__)
+
+# Assistant Client for PyTgCalls
+assistant_client = Client(
+    "stark_assistant",
+    api_id=Config.API_ID,
+    api_hash=Config.API_HASH,
+    session_string=Config.ASSISTANT_STRING,
+    in_memory=True
+)
+
+# PyTgCalls instance
+call_py = PyTgCalls(assistant_client)
+
+class CallManager:
+    """Manages active voice calls"""
+    
+    active_calls = {}  # chat_id -> call data
+    
+    @staticmethod
+    async def start():
+        """Start PyTgCalls client"""
+        await call_py.start()
+        logger.info("🎵 PyTgCalls client started")
+    
+    @staticmethod
+    async def stop():
+        """Stop PyTgCalls client"""
+        await call_py.stop()
+        logger.info("🎵 PyTgCalls client stopped")
+    
+    @staticmethod
+    def get_call(chat_id: int):
+        """Get call instance for chat"""
+        return call_py.get_active_call(chat_id)
